7d41d1ae6800bd7e7273e48f63bc725baa254c9b
amarnath-nitt <amarnath97.nitt@gmail.com>
2022-09-16 08:26:17 +0530

    updated

---
+++ DataStructure/AddTwoNumbers.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a=12 , b=5;
+    int sum = a + b;
+    cout << "Sum: " << a << " + " << b << " = "<< sum<<endl;
+    return 0; 
+}
